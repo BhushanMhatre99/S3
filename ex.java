c67572a4f483d1e329a790e0670e2c56a2bad5bf
Bhushan <imbhush99@gmail.com>
2019-01-27 15:01:22 +0530

    project complete!

---
+++ ex.java
@@ -0,0 +1,7 @@
+class bhushan
+{
+public static void main(String a[])
+{
+System.out.println("hello world");
+}
+}
---
+++ file_upload_test.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath =  files.filetoupload.name;
+
+      fs.readFile(files.filetoupload.path, function (err, data) {
+  // ...
+  //var newPath = __dirname + "/uploads/uploadedFileName";
+  fs.writeFile(newpath, data, function (err) {
+
+  });
+});
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
